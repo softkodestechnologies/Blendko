21bcbb35b60d589174b774394abd6a9b9cfa17ba
Faad Abu <faadabu@gmail.com>
2024-05-27 16:35:55 +0100

    Implemented Product Route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 
-
+const productRoutes = require('./product');
 
 
 const router = Router();
 
 
-
+router.use('/product', productRoutes);
 
 
 module.exports = router;
