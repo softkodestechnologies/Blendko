a8edf1d3ecd7763bf25270d3b4388d5b69ac930a
Faad Abu <faadabu@gmail.com>
2024-05-27 16:44:48 +0100

    Implemented Order Route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 
-
+orderRoutes = require('./order');
 
 
 const router = Router();
 
 
-
+router.use('/order', orderRoutes);
 
 
 module.exports = router;
---
+++ server/routes/order.js
@@ -0,0 +1,38 @@
+const { Router } = require('express');
+const router = Router();
+
+const {
+  myOrders,
+  getSingleOrder,
+  allOrders,
+  updateOrder,
+  deleteOrder,
+  createOrder,
+} = require('../controllers/order.controller');
+
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
+router.post('/new', isAuthenticatedUser, createOrder);
+router.get('/myorders', isAuthenticatedUser, myOrders);
+router.get('/:id', isAuthenticatedUser, getSingleOrder);
+
+router.get(
+  '/admin/all',
+  isAuthenticatedUser,
+  authorizeRoles('admin'),
+  allOrders
+);
+router.put(
+  '/admin/:id',
+  isAuthenticatedUser,
+  authorizeRoles('admin'),
+  updateOrder
+);
+router.delete(
+  '/admin/:id',
+  isAuthenticatedUser,
+  authorizeRoles('admin'),
+  deleteOrder
+);
+
+module.exports = router;
