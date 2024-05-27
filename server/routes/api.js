0bfce3c86810d6f004e547390839866dad6d4626
Faad Abu <faadabu@gmail.com>
2024-05-27 16:17:29 +0100

    Implemented User Routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 
-
+const userRoutes = require('./user');
 
 
 const router = Router();
 
 
-
+router.use('/', userRoutes)
 
 
 module.exports = router;
---
+++ server/routes/user.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  forgotPassword,
+  resetPassword,
+  getUserProfile,
+  updatePassword,
+  updateProfile,
+  getAllUsers,
+  getUserDetails,
+  updateUserRole,
+  deleteUser,
+  getCartItems,
+  addToCart,
+  removeFromCart,
+  deleteCartItem,
+  googleLogin,
+  googleRegister,
+} = require('../controllers/user.controller');
+
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
+router.route('/register').post(registerUser);
+router.route('/googleregister').post(googleRegister);
+router.route('/login').post(loginUser);
+router.route('/googlelogin').post(googleLogin);
+router.route('/logout').get(logoutUser);
+
+router.route('/password/forgot').post(forgotPassword);
+router.route('/password/reset/:token').put(resetPassword);
+
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+router.route('/cart').post(isAuthenticatedUser, addToCart)
+router.route('/cart').get(isAuthenticatedUser, getCartItems)
+router.route('/cart/:id').patch(isAuthenticatedUser, removeFromCart)
+router.route('/cart/:id').delete(isAuthenticatedUser, deleteCartItem)
+
+
+router
+  .route('/admin/users')
+  .get(isAuthenticatedUser, authorizeRoles('admin'), getAllUsers);
+
+router
+  .route('/admin/user/:id')
+  .get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
+  .put(isAuthenticatedUser, authorizeRoles('admin'), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser);
+
+module.exports = router;
