c2a9d799cbd60d4e15c0e92d20092c98f96a7988
Faad Abu <faadabu@gmail.com>
2024-05-27 17:14:27 +0100

    Implemented Category Route

---
+++ server/routes/category.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+
+const { getCategories, getCategoryByName } = require('../controllers/category.controller');
+
+const router = Router();
+
+router.get('/', getCategories);
+router.get('/:name', getCategoryByName);
