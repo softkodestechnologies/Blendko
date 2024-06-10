5b6ce72b92b4f3b08d8a47c94fdeb2c593a7bf07
Faad Abu <faadabu@gmail.com>
2024-06-10 11:12:51 +0100

    Changed Cart Design

--- client/src/components/ui/Cart.tsx
+++ client/src/components/ui/Cart.tsx
@@ -19,32 +19,34 @@ interface CartProps {
 
 const Cart: FC<CartProps> = ({ cartOpen, toggleCart, cartItems }) => {
   return (
-    <>
+    <div className='cart-over'>
       {cartOpen && <div className="overlay" onClick={toggleCart}></div>}
-      <div className={`cart-modal ${cartOpen ? 'open' : ''}`}>
-        <div className="cart-header">
-          <h2>Added to Bag</h2>
-          <button className="close-button" title="close" onClick={toggleCart}><IoClose /></button>
-        </div>
-        <div className="cart-items">
-          {cartItems.map(item => (
-            <div key={item.id} className="cart-item">
-              <Image src={item.image} alt={item.title} width={80} height={80} />
-              <div>
-                <h3>{item.title}</h3>
-                <p>{item.category}</p>
-                <p>Size {item.size}</p>
-                <p>${item.price}</p>
+      <div className="container">
+        <div className={`cart-modal ${cartOpen ? 'open' : ''}`}>
+          <div className="cart-header">
+            <h2>Added to Bag</h2>
+            <button className="close-button" title="close" onClick={toggleCart}><IoClose /></button>
+          </div>
+          <div className="cart-items">
+            {cartItems.map(item => (
+              <div key={item.id} className="cart-item">
+                <Image src={item.image} alt={item.title} width={80} height={80} />
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.category}</p>
+                  <p>Size {item.size}</p>
+                  <p>${item.price}</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div className="cart-actions">
-          <button className="view-bag-button"><Link href="/view-bag">View Bag ({cartItems.length})</Link></button>
-          <button className="checkout-button">Checkout</button>
+            ))}
+          </div>
+          <div className="cart-actions">
+            <button className="view-bag-button"><Link href="/view-bag">View Bag ({cartItems.length})</Link></button>
+            <button className="checkout-button">Checkout</button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
