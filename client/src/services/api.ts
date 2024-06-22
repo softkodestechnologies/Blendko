718b8959889bbadfe65c3a2a27ea3504a8fd22c4
Faad Abu <faadabu@gmail.com>
2024-06-22 14:17:29 +0100

    changes to service/api

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,13 +1,13 @@
 // services/api.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-let baseUrl;
+let baseUrl = 'https://blendko.onrender.com/api/v1';
 
-if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://localhost:8080/api/v1/';
-} else {
-  baseUrl = 'https://blendko.onrender.com/api/v1/';
-}
+// if (process.env.NODE_ENV === 'development') {
+//   baseUrl = 'http://localhost:8080/api/v1/';
+// } else {
+//   baseUrl = 'https://blendko.onrender.com/api/v1/';
+// }
 
 export const blendkoApi = createApi({
   reducerPath: 'blendkoApi',
