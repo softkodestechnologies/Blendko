abb087684f57e9e92789ce0c014421629aaa69ce
Faad Abu <faadabu@gmail.com>
2024-06-25 22:03:38 +0100

    updated the API Features

--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -6,9 +6,12 @@ class ApiFeatures {
   }
 
   search() {
+    if(this.queryStr.keyword === "All Sale") {
+      this.queryStr.keyword = ''
+    }
     const keyword = this.queryStr.keyword
       ? {
-          name: {
+          features: {
             $regex: this.queryStr.keyword,
             $options: 'i',
           },
@@ -23,38 +26,38 @@ class ApiFeatures {
   }
 
   filter() {
-const queryCopy = {...this.queryStr };
+    const queryCopy = {...this.queryStr };
 
-const removeFields = ['keyword', 'limit', 'page', 'pp', 'ort'];
-removeFields.forEach((el) => delete queryCopy[el]);
+    const removeFields = ['keyword', 'limit', 'page', 'pp', 'ort'];
+    removeFields.forEach((el) => delete queryCopy[el]);
 
-// Split colors into an array if it's a comma-separated string
-if (queryCopy.colors) {
-  queryCopy.colors = queryCopy.colors.split(',');
-}
-
-const query = {};
-
-for (const key in queryCopy) {
-  if (queryCopy.hasOwnProperty(key)) {
-    if (key === 'colors') {
-      query[key] = { $in: queryCopy[key] };
-    } else if (key === 'sizes') {
-      query[key] = { $in: queryCopy[key].split(',') };
-    } else if (key === 'dress_style') {
-      query[key] = { $in: queryCopy[key].split(',') };
-    } else if (key === 'fashion_collection') {
-      query[key] = { $in: queryCopy[key].split(',') };
-    } else if (key === 'technology') {
-      query[key] = { $in: queryCopy[key].split(',') };
-    }   else {
-      for (const operator in queryCopy[key]) {
-        if (queryCopy[key].hasOwnProperty(operator)) {
-          query[key] = { [`$${operator}`]: queryCopy[key][operator] };
-        }
-      }
+    // Split colors into an array if it's a comma-separated string
+    if (queryCopy.colors) {
+      queryCopy.colors = queryCopy.colors.split(',');
     }
-  }
+
+    const query = {};
+
+    for (const key in queryCopy) {
+      if (queryCopy.hasOwnProperty(key)) {
+        if (key === 'colors') {
+          query[key] = { $in: queryCopy[key] };
+        } else if (key === 'sizes') {
+          query[key] = { $in: queryCopy[key].split(',') };
+        } else if (key === 'dress_style') {
+          query[key] = { $in: queryCopy[key].split(',') };
+        } else if (key === 'fashion_collection') {
+          query[key] = { $in: queryCopy[key].split(',') };
+        } else if (key === 'technology') {
+          query[key] = { $in: queryCopy[key].split(',') };
+        }   else {
+          for (const operator in queryCopy[key]) {
+            if (queryCopy[key].hasOwnProperty(operator)) {
+              query[key] = { [`$${operator}`]: queryCopy[key][operator] };
+            }
+          }
+        }
+    }
 }
 
 this.query = this.query.find(query);
