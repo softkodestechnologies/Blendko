1a0e85334c9abcb36f2d7068c5a117253e52ee5e
Faad Abu <faadabu@gmail.com>
2024-07-29 23:50:09 +0100

    changed baseUrl

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,13 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-
-let baseUrl;
-
-if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://localhost:8080/api/v1/';
-} else {
-  baseUrl = 'https://blendko.onrender.com/api/v1';
-}
+let baseUrl = 'https://blendko.onrender.com/api/v1';
 
 export const blendkoApi = createApi({
   reducerPath: 'blendkoApi',
