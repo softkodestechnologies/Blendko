6f084ea21a995008733a8944f7b4307b8ad32278
Faad Abu <faadabu@gmail.com>
2024-08-01 11:25:18 +0100

    revert to former structure

--- client/src/services/userSlice.ts
+++ client/src/services/userSlice.ts
@@ -10,13 +10,8 @@ interface UserState {
   };
 }
 
-let baseUrl: string;
+let baseUrl = 'https://blendko.onrender.com/api/v1';
 
-if (process.env.NODE_ENV === 'development') {
-  baseUrl = process.env.NEXT_PUBLIC_DEV_URL!;
-} else {
-  baseUrl = process.env.NEXT_PUBLIC_PROD_URL!;
-}
 
 
 let user;
