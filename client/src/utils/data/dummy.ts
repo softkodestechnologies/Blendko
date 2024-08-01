035cfba129057c2d8581ac4e1f447c2b7461db42
duanedave <duanedave17@gmail.com>
2024-08-02 00:12:32 +0100

    chore: Add dummy data for navigation links

---
+++ client/src/utils/data/dummy.ts
@@ -0,0 +1,43 @@
+export const navLinks = [
+  {
+    title: 'Home',
+    url: '/',
+    subMenu: [],
+  },
+  {
+    title: 'New Arrival',
+    url: '/shop',
+    subMenu: [],
+  },
+  {
+    title: 'Categories',
+    url: '',
+    subMenu: [
+      {
+        title: 'Bags',
+        url: '/shop?category=Bags',
+      },
+      {
+        title: 'Kids',
+        url: '/shop?category=Kids',
+      },
+      {
+        title: 'Jewelries',
+        url: '/shop?category=Jewelries',
+      },
+      {
+        title: 'Men’s Fashion',
+        url: '/shop?category=Men’s Fashion',
+      },
+      {
+        title: 'Women’s Fashion',
+        url: '/shop?category=Women’s Fashion',
+      },
+    ],
+  },
+  {
+    title: 'Customization',
+    url: '/shop',
+    subMenu: [],
+  },
+];
--- client/src/utils/helpers/checkUrl.ts
+++ client/src/utils/helpers/checkUrl.ts
@@ -1,30 +1,30 @@
 type ArgType = {
-    key: string;
-    value: string | null;
+  key: string;
+  value: string | null;
+};
+
+function checkURL(entries: ArgType[]) {
+  const hash: { [key: string]: string } = {};
+
+  entries.forEach((entry) => {
+    if (entry.value !== null && entry.value !== '') {
+      hash[entry.key] = entry.value;
+    }
+  });
+
+  const search = Object.keys(hash)
+    .map((key) => {
+      const value = hash[key];
+      return `${key}=${value}`;
+    })
+    .join('&');
+
+  const url = search ? `?${search}` : '';
+
+  return {
+    url,
+    hash,
   };
-  
-  function checkURL(entries: ArgType[]) {
-    const hash: { [key: string]: string } = {};
-  
-    entries.forEach((entry) => {
-      if (entry.value !== null && entry.value !== '') {
-        hash[entry.key] = entry.value;
-      }
-    });
-  
-    const search = Object.keys(hash)
-      .map((key) => {
-        const value = hash[key];
-        return `${key}=${value}`;
-      })
-      .join('&');
-  
-    const url = search ? `?${search}` : '';
-  
-    return {
-      url,
-      hash,
-    };
-  }
-  
-  export default checkURL;
+}
+
+export default checkURL;
