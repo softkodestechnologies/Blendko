3c0952fe962ad07491727924739558c3b075e75e
duanedave <duanedave17@gmail.com>
2024-08-05 19:52:14 +0100

    refactor: Add CustomizeSection component for home page customization section

---
+++ client/src/components/home/customize/CustomizeSection.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+
+import styles from './customize.module.css';
+import layoutStyles from '../home.module.css';
+
+import Customize from './Customize';
+
+function CustomizeSection() {
+  return (
+    <section className={`${layoutStyles.container}`}>
+      <div className={`${styles.customize_wrapper}`}>
+        <div className={`flex flex-col center ${styles.mobile_customize}`}>
+          <h2>Customize Your Style</h2>
+
+          <p>
+            Transform your wardrobe with our customization tools. Choose
+            fabrics, add unique touches, and express your style!
+          </p>
+
+          <Customize />
+
+          <Link href="/customize" className="flex center">
+            EXPLORE
+          </Link>
+        </div>
+
+        <div className={`grid ${styles.desktop_customize}`}>
+          <Customize />
+
+          <div className={`flex flex-col align-x ${styles.right}`}>
+            <h2>Customize Your Style</h2>
+
+            <p>
+              Transform your wardrobe with our customization tools. Choose
+              fabrics, add unique touches, and express your style!
+            </p>
+
+            <Link href="/customize" className="flex center">
+              Start Customising
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default CustomizeSection;
