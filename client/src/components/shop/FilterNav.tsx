fd44c2107f61c88ce9b5ac56c2cb2634a9bbd912
duanedave <duanedave17@gmail.com>
2024-08-23 18:24:03 +0100

    refactor: Improve FilterNav component code organization and readability

--- client/src/components/shop/FilterNav.tsx
+++ client/src/components/shop/FilterNav.tsx
@@ -2,34 +2,46 @@ import React, { useState } from 'react';
 import styles from './FilterNav.module.css';
 
 interface FilterNavProps {
-    onSearch: (key: string, value: string) => void;
-    menuItems: string[];
-    activeIndex: React.SetStateAction<number>;
-    setIndex: (index: React.SetStateAction<number>) => void;
-  }
+  onSearch: (key: string, value: string) => void;
+  menuItems: string[];
+  activeIndex: React.SetStateAction<number>;
+  setIndex: (index: React.SetStateAction<number>) => void;
+}
 
-const FilterNav: React.FC<FilterNavProps> = ({ onSearch, menuItems, activeIndex, setIndex }) => {
- 
-
+const FilterNav: React.FC<FilterNavProps> = ({
+  onSearch,
+  menuItems,
+  activeIndex,
+  setIndex,
+}) => {
   const clickedSearch = (index: React.SetStateAction<number>, item: string) => {
     setIndex(index);
 
-    if(item === 'Shoes'){
-      item = 'Shoe'
+    if (item === 'Shoes') {
+      item = 'Shoe';
     }
     onSearch('keyword', item);
-  }
+  };
 
   return (
     <div className={styles.navContainer}>
       {menuItems.map((item, index) => (
         <div
           key={index}
-          className={`${styles.navItem} ${activeIndex === index ? styles.active : ''}`}
-          onClick={() => clickedSearch(index, item)}>
+          className={`${styles.navItem} ${
+            activeIndex === index ? styles.active : ''
+          }`}
+          onClick={() => clickedSearch(index, item)}
+        >
           {item}
-          {<div className={styles.underline} style={{ transform: `translateX(${activeIndex}%)` }}/>}
-        </div>))}
+          {
+            <div
+              className={styles.underline}
+              style={{ transform: `translateX(${activeIndex}%)` }}
+            />
+          }
+        </div>
+      ))}
     </div>
   );
 };
