bbb169a299846ee9ac5970c714eb3353997cd00e
duanedave <duanedave17@gmail.com>
2024-08-28 21:53:15 +0100

    refactor: Update PriceSlider component for shop page
    - Add state for expanded and priceRange in PriceSlider.tsx
    - Implement handleSliderChange function to update priceRange state
    - Update Slider component to use priceRange state for defaultValue
    - Add price labels to display the selected price range
    - Improve code organization and readability

--- client/src/components/shop/filterComponents/PriceSlider.tsx
+++ client/src/components/shop/filterComponents/PriceSlider.tsx
@@ -1,14 +1,24 @@
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import { useState } from 'react';
 
+import 'rc-slider/assets/index.css';
 import styles from './styles.module.css';
 
 import Accordion from '@/components/ui/accordion/Accordion';
-import { ChevronIcon, CheckMark } from '../../../../public/svg/icon';
+import { ChevronIcon } from '../../../../public/svg/icon';
 
 function PriceSlider() {
+  const [expanded, setExpanded] = useState(true);
+  const [priceRange, setPriceRange] = useState([200, 800]);
+
+  const handleSliderChange = (value: any) => {
+    setPriceRange(value);
+  };
+
   return (
     <Accordion
+      toggleOpen={expanded}
+      setToggleOpen={() => setExpanded(!expanded)}
       className={`${styles.sidebar_filter}`}
       head={
         <>
@@ -22,19 +32,28 @@ function PriceSlider() {
           <Slider
             range
             min={0}
-            keyboard
             max={1000}
-            // defaultValue={[Number(priceGTE) || 300, Number(priceLTE) || 600]}
-            defaultValue={[300, 600]}
+            defaultValue={priceRange}
             className={`${styles.slider}`}
-            onChangeComplete={(value) => console.log(value)}
-            // onAfterChange={(value) => handlePriceRange(value)}
+            onChange={handleSliderChange}
             styles={{
               handle: { backgroundColor: '#000', border: 'none' },
               track: { backgroundColor: '#000' },
+              
             }}
             ariaLabelForHandle={['Lower price limit', 'Upper price limit']}
           />
+          <div
+            className={`flex full-width space-between ${styles.price_labels}`}
+          >
+            <span style={{ left: `${(priceRange[0] / 1000) * 100}%` }}>
+              ${priceRange[0]}
+            </span>
+
+            <span style={{ left: `${(priceRange[1] / 1000) * 100}%` }}>
+              ${priceRange[1]}
+            </span>
+          </div>
         </div>
       }
     />
