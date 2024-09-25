781eb0640f29ee4b6aafd3d12c726b46dc93ab34
Faad Abu <faadabu@gmail.com>
2024-09-25 15:59:08 +0100

    refactor: Job, discounts fixes

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -168,6 +168,7 @@ exports.setJobAlert = catchAsyncErrors(async (req, res, next) => {
 
 
 exports.getAllJobApplications = catchAsyncErrors(async (req, res, next) => {
+    console.log('this is the requested route', req.params.id)
     const Applications = await JobApplication.find()
       .populate('job', 'title');
   
@@ -177,7 +178,35 @@ exports.getAllJobApplications = catchAsyncErrors(async (req, res, next) => {
     });
 });
 
+exports.getJobApplicationsByJobId = catchAsyncErrors(async (req, res, next) => {
+    const jobId = req.params.jobId;
+    console.log('Job Id', jobId)
+
+    const applications = await JobApplication.find({ job: jobId })
+
+    console.log('application should not be empty', applications)
+
+    if (!applications) {
+        return next(new ErrorHandler('No applications found for this job', 404));
+    }
+
+    if(applications.length === 0) {
+        return res.status(404). json({
+            success: true,
+            applications
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        applications
+    });
+});
+
+
 exports.getSingleJobApplication = catchAsyncErrors(async (req, res, next) => {
+    console.log('this is id', req.params.id)
+
   const application = await JobApplication.findById(req.params.id)
     .populate('job', 'title');
 
